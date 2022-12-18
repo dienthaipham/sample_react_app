0a3d8bf4d5fb280f32a8b99b916ea0523c9408e6
dienthaipham <119146703+dienthaipham@users.noreply.github.com>
2022-12-18 13:52:06 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return <div className="App">Hello world</div>;
+    return <div className="App">Hello world Pham Thai Dien</div>;
 }
 
 export default App;
