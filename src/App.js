981b816641446a836ee58b0f520d56159cbd837f
Pham Thai Dien <dienthaipham2022@gmail.com>
2022-12-18 13:53:00 +0700

    abc

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return <div className="App">Hello world Pham Thai Dien</div>;
+    return (
+        <div className="App">
+            Hello world Pham Thai Dien
+            <span>Ngo Tan Hung</span>
+        </div>
+    );
 }
 
 export default App;
